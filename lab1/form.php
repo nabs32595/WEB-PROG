e1284c12a23cca6799c348cc4ba58492eb9e3c0f
Nabil Khalid Alkaff <nabil.k.alkaff@gmail.com>
2017-06-07 09:31:22 +0800

    test

---
+++ lab1/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+Welcome <?php	echo $_POST["username"]; ?>
+<br>
+Your email is:  <?php	echo $_POST["email"]; ?>
+<br>
+Your password is:  <?php	echo $_POST["pass"]; ?>
+
+</body>
+</html>
+
+
+
---
+++ lab1/output.php
@@ -0,0 +1,9 @@
+
+<?php	
+$name = "azlan3";
+
+echo "My name is: " . $_POST["username"] ;
+
+echo "hi " . $name . " bye2"; 
+
+?>
